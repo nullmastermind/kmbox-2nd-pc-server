d408dad5dacdafe7b527db19cce264e0d40085be
monkjiro <thenullmastermind@gmail.com>
2024-06-11 21:13:05 +0700

    feat: Add UDP server to the main module and refactor argument types in my_kmnet module

--- main.py
+++ main.py
@@ -1,24 +1,40 @@
-import time
+import socket
 
 import my_kmnet
 
+
+def udp_server(host="127.0.0.1", port=12345):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    server_address = (host, port)
+    sock.bind(server_address)
+
+    print(f"UDP server up and listening on {host}:{port}")
+
+    while True:
+        data, address = sock.recvfrom(1024)
+        data_str = data.decode()
+        commands = data_str.split("\n")
+
+        for command in commands:
+            try:
+                parts = command.split(",")
+                call_type = parts[0]
+                function = parts[1]
+                params = parts[2:]
+                call_result = my_kmnet.my_kmnet_functions[function](*params)
+                if call_type == "call_return":
+                    sock.sendto(bytes(str(call_result), "utf-8"), address)
+            except Exception as call_error:
+                print(call_error)
+
+
 if __name__ == "__main__":
     my_kmnet.init("192.168.2.188", "8357", "73E55C53")
-    my_kmnet.monitor(10000)
+    my_kmnet.monitor("10000")
 
     try:
-        my_kmnet.mask_left(1)
-        while 1:
-            if my_kmnet.isdown_left():
-                print("left button is down")
-            if my_kmnet.isdown_right():
-                print("right button is down")
-            if my_kmnet.isdown_middle():
-                print("middle button is down")
-            if my_kmnet.isdown_side1():
-                print("side1 button is down")
-            if my_kmnet.isdown_side2():
-                print("side2 button is down")
-            time.sleep(0.5)
-    except:
+        udp_server()
+    except Exception as e:
+        print(e)
         my_kmnet.unmask_all()
--- my_kmnet.py
+++ my_kmnet.py
@@ -6,44 +6,44 @@ def init(ip: str, port: str, mac: str) -> None:
     kmNet.init(ip, port, mac)
 
 
-def move(x: int, y: int) -> None:
-    kmNet.move(x, y)
+def move(x: str, y: str) -> None:
+    kmNet.move(int(x), int(y))
 
 
-def left(isdown: int) -> None:
-    kmNet.left(isdown)
+def left(isdown: str) -> None:
+    kmNet.left(int(isdown))
 
 
-def middle(isdown: int) -> None:
-    kmNet.middle(isdown)
+def middle(isdown: str) -> None:
+    kmNet.middle(int(isdown))
 
 
-def right(isdown: int) -> None:
-    kmNet.right(isdown)
+def right(isdown: str) -> None:
+    kmNet.right(int(isdown))
 
 
-def wheel(wheel_value: int) -> None:
-    kmNet.wheel(wheel_value)
+def wheel(wheel_value: str) -> None:
+    kmNet.wheel(int(wheel_value))
 
 
-def move_auto(x: int, y: int, time_ms: int) -> None:
-    kmNet.move_auto(x, y, time_ms)
+def move_auto(x: str, y: str, time_ms: str) -> None:
+    kmNet.move_auto(int(x), int(y), int(time_ms))
 
 
-def move_beizer(x: int, y: int, ms: int, x1: int, y1: int, x2: int, y2: int) -> None:
-    kmNet.move_beizer(x, y, ms, x1, y1, x2, y2)
+def move_beizer(x: str, y: str, ms: str, x1: str, y1: str, x2: str, y2: str) -> None:
+    kmNet.move_beizer(int(x), int(y), int(ms), int(x1), int(y1), int(x2), int(y2))
 
 
-def keydown(vkey: int) -> None:
-    kmNet.keydown(vkey)
+def keydown(vkey: str) -> None:
+    kmNet.keydown(int(vkey))
 
 
-def keyup(vkey: int) -> None:
-    kmNet.keyup(vkey)
+def keyup(vkey: str) -> None:
+    kmNet.keyup(int(vkey))
 
 
-def monitor(port: int) -> None:
-    kmNet.monitor(port)
+def monitor(port: str) -> None:
+    kmNet.monitor(int(port))
 
 
 def isdown_left() -> bool:
@@ -66,71 +66,107 @@ def isdown_side2() -> bool:
     return kmNet.isdown_side2()
 
 
-def isdown_keyboard(vkey: int) -> bool:
-    return kmNet.isdown_keyboard(vkey)
+def isdown_keyboard(vkey: str) -> bool:
+    return kmNet.isdown_keyboard(int(vkey))
 
 
 # Mask Functions
-def mask_left(enable: int) -> None:
-    kmNet.mask_left(enable)
+def mask_left(enable: str) -> None:
+    kmNet.mask_left(int(enable))
 
 
-def mask_middle(enable: int) -> None:
-    kmNet.mask_middle(enable)
+def mask_middle(enable: str) -> None:
+    kmNet.mask_middle(int(enable))
 
 
-def mask_right(enable: int) -> None:
-    kmNet.mask_right(enable)
+def mask_right(enable: str) -> None:
+    kmNet.mask_right(int(enable))
 
 
-def mask_side1(enable: int) -> None:
-    kmNet.mask_side1(enable)
+def mask_side1(enable: str) -> None:
+    kmNet.mask_side1(int(enable))
 
 
-def mask_side2(enable: int) -> None:
-    kmNet.mask_side2(enable)
+def mask_side2(enable: str) -> None:
+    kmNet.mask_side2(int(enable))
 
 
-def mask_wheel(enable: int) -> None:
-    kmNet.mask_wheel(enable)
+def mask_wheel(enable: str) -> None:
+    kmNet.mask_wheel(int(enable))
 
 
-def mask_x(enable: int) -> None:
-    kmNet.mask_x(enable)
+def mask_x(enable: str) -> None:
+    kmNet.mask_x(int(enable))
 
 
-def mask_y(enable: int) -> None:
-    kmNet.mask_y(enable)
+def mask_y(enable: str) -> None:
+    kmNet.mask_y(int(enable))
 
 
-def mask_keyboard(vkey: int, enable: int) -> None:
-    kmNet.mask_keyboard(vkey, enable)
+def mask_keyboard(vkey: str, enable: str) -> None:
+    kmNet.mask_keyboard(int(vkey), int(enable))
 
 
-def unmask_keyboard(vkey: int) -> None:
-    kmNet.unmask_keyboard(vkey)
+def unmask_keyboard(vkey: str) -> None:
+    kmNet.unmask_keyboard(int(vkey))
 
 
 def unmask_all() -> None:
     kmNet.unmask_all()
 
 
-# Miscellaneous Functions
 def reboot() -> None:
     kmNet.reboot()
 
 
-def setip_port(ip: str, port: int) -> None:
-    kmNet.setip_port(ip, port)
+def setip_port(ip: str, port: str) -> None:
+    kmNet.setip_port(ip, int(port))
 
 
-def lcd_color(rgb565: int) -> None:
-    kmNet.lcd_color(rgb565)
+def lcd_color(rgb565: str) -> None:
+    kmNet.lcd_color(int(rgb565))
 
 
-def lcd_picture(buff_128_160: bytes) -> None:
-    kmNet.lcd_picture(buff_128_160)
+def lcd_picture(buff_128_160: str) -> None:
+    kmNet.lcd_picture(bytes(buff_128_160, "utf-8"))
 
 
-def lcd_picture_bottom(buff_128_80: bytes) -> None:
-    kmNet.lcd_picture_bottom(buff_128_80)
+def lcd_picture_bottom(buff_128_80: str) -> None:
+    kmNet.lcd_picture_bottom(bytes(buff_128_80, "utf-8"))
+
+
+my_kmnet_functions = {
+    "init": init,
+    "move": move,
+    "left": left,
+    "middle": middle,
+    "right": right,
+    "wheel": wheel,
+    "move_auto": move_auto,
+    "move_beizer": move_beizer,
+    "keydown": keydown,
+    "keyup": keyup,
+    "monitor": monitor,
+    "isdown_left": isdown_left,
+    "isdown_middle": isdown_middle,
+    "isdown_right": isdown_right,
+    "isdown_side1": isdown_side1,
+    "isdown_side2": isdown_side2,
+    "isdown_keyboard": isdown_keyboard,
+    "mask_left": mask_left,
+    "mask_middle": mask_middle,
+    "mask_right": mask_right,
+    "mask_side1": mask_side1,
+    "mask_side2": mask_side2,
+    "mask_wheel": mask_wheel,
+    "mask_x": mask_x,
+    "mask_y": mask_y,
+    "mask_keyboard": mask_keyboard,
+    "unmask_keyboard": unmask_keyboard,
+    "unmask_all": unmask_all,
+    "reboot": reboot,
+    "setip_port": setip_port,
+    "lcd_color": lcd_color,
+    "lcd_picture": lcd_picture,
+    "lcd_picture_bottom": lcd_picture_bottom,
+}
