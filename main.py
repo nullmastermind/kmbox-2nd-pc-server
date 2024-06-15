bde42188b10b643cad59ca2340a67d85f3b87539
monkjiro <thenullmastermind@gmail.com>
2024-06-15 10:34:16 +0700

    refactor: Change server from UDP to TCP in main.py

--- main.py
+++ main.py
@@ -1,37 +1,52 @@
 import os
-
 from dotenv import load_dotenv
 import socket
-
 import my_kmnet
 
 load_dotenv()
 
 
-def udp_server(host="0.0.0.0", port=12345):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+def tcp_server(host="0.0.0.0", port=12345):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_address = (host, port)
     sock.bind(server_address)
+    sock.listen(1)  # Listen for incoming connections
 
-    print(f"UDP server up and listening on {host}:{port}")
+    print(f"TCP server up and listening on {host}:{port}")
 
     while True:
-        data, address = sock.recvfrom(2048)
-        data_str = data.decode()
-        commands = data_str.split("\n")
+        connection, client_address = sock.accept()
+        try:
+            print(f"Connection from {client_address}")
 
-        for command in commands:
-            try:
-                parts = command.split(",")
-                call_type = parts[0]
-                function = parts[1]
-                params = parts[2:]
-                call_result = my_kmnet.my_kmnet_functions[function](*params)
-                if call_type == "call_return":
-                    sock.sendto(bytes(str(call_result), "utf-8"), address)
-            except Exception as call_error:
-                print("call_error: {}".format(call_error))
+            while True:
+                data = connection.recv(2048)
+                if not data:
+                    break
+                data_str = data.decode()
+                commands = data_str.split("\n")
+
+                for command in commands:
+                    try:
+                        parts = command.split(",")
+                        call_type = parts[0]
+                        function = parts[1]
+                        params = parts[2:]
+                        call_result = my_kmnet.my_kmnet_functions[function](*params)
+                        # call_result = "Ok"
+                        if call_type == "call_return":
+                            connection.sendall(bytes(str(call_result), "utf-8"))
+                        else:
+                            connection.sendall(b"OK")
+                    except Exception as call_error:
+                        print("call_error: {}".format(call_error))
+            my_kmnet.unmask_all()
+        except Exception as _e:
+            # print("connection error:", _e)
+            my_kmnet.unmask_all()
+        finally:
+            print("Connection closed")
+            connection.close()
 
 
 if __name__ == "__main__":
@@ -41,7 +56,8 @@ if __name__ == "__main__":
     my_kmnet.monitor("10000")
 
     try:
-        udp_server()
+        tcp_server()
     except Exception as e:
-        print(e)
+        print("error:", e)
+    finally:
         my_kmnet.unmask_all()
